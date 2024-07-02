cb781c27a2f7b437f9703d5c8d7dc342405f4fc9
Marcio David <marciodavid81@gmail.com>
2024-07-02 11:18:16 -0300

    fix: error fechar modal

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const botaoAbrir = document.querySelector('[data-modal="abrir"]');
 const botaoFechar = document.querySelector('[data-modal="fechar"]');
 const containerModal = document.querySelector('[data-modal="container"]');
+const botaoEnviar = document.querySelector('[data-modal="enviar"]');
 
 if(botaoAbrir && botaoFechar && containerModal){
     function abrirModal(event){
@@ -21,4 +22,5 @@ if(botaoAbrir && botaoFechar && containerModal){
     botaoAbrir.addEventListener('click', abrirModal);
     botaoFechar.addEventListener('click', fecharModal);
     containerModal.addEventListener('click', cliqueForaModal);
+    botaoEnviar.addEventListener('click', fecharModal);
 }
